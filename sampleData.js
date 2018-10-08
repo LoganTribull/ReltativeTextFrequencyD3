2ccace9e4366c4c2b865f724b319648f3b9b8e77
Logan Tribull <logantribull@Logans-MacBook-Pro.local>
2018-10-07 21:09:53 -0700

    sample data of what relative word frequencies will look like

---
+++ sampleData.js
@@ -0,0 +1,18 @@
+
+data = [
+  {word:"dog", ratio:3},
+  {word:"establishment", ratio:5},
+  {word:"test", ratio:null},
+  {word:"rather", ratio:8},
+  {word:"gym", ratio:1},
+  {word:"exceptional", ratio:null},
+  {word:"effort", ratio:4},
+  {word:"appartment", ratio:4},
+  {word:"city", ratio:12},
+  {word:"sign", ratio:8},
+  {word:"radio", ratio:9},
+  {word:"france", ratio:10},
+  {word:"canada", ratio:2},
+  {word:"peru", ratio:null},
+  {word:"desk", ratio:3}
+]
