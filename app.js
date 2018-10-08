920d31eaba7e29f2597370aad2193f5b9fec7410
Logan Tribull <logantribull@Logans-MacBook-Pro.local>
2018-10-07 22:15:27 -0700

    simple removing of exit when button is clicked from active word list and bars

--- app.js
+++ app.js
@@ -1,40 +1,56 @@
 const svgWidth = 500
 const svgHeight = 500
 const svgPadding = 50
-data = data.filter(d => d.ratio !== null)
+activeData= data.filter(d => d.ratio !== null)
+inactiveData = []
+
+inactivateWord= event => {
+  inactiveData.push(...activeData.filter(d=>d.word === event.word))
+  activeData = activeData.filter(d => d.word !== event.word)
+  activeWordList
+    .data(activeData, d=>d.word)
+    .exit()
+    .remove()
+    .enter()
+    .append("button")
+    .text(d=>d.word)
+    .on("click", inactivateWord)
+
+  bars
+    .data(activeData)
+    .exit()
+    .remove()
+}
 
 var svg = d3.select("svg")
           .attr("width", svgWidth + svgPadding)
           .attr("height", svgHeight + svgPadding)
           .style("border", "1px solid blue")
 
-var activeWordList = d3.select("#active-word-list")
+var activeWordList = d3.select("#active-word-list").
+selectAll("button").
+data(activeData, d=>d.word)
+.enter()
+.append("button")
+.text(d=>d.word)
+.on("click", inactivateWord)
+
+
+var inactiveWordList = d3.select("#active-word-list").selectAll("button").data(inactiveData, d=>d.word)
 
 var xScale = d3.scalePoint()
-              .domain(data.map(d=>d.word))
+              .domain(activeData.map(d=>d.word))
               .range([0, svgWidth])
 
 var yScale = d3.scaleLinear()
-              .domain(d3.extent(data, d=>d.ratio))
+              .domain(d3.extent(activeData, d=>d.ratio))
               .range([0, svgHeight])
 
 // Filter data for null values
 
-removeWord = event => {
-  
-}
-
-activeWordList
-            .selectAll("button")
-            .data(data, d=>d.word)
-            .enter()
-            .append("button")
-            .text(d=>d.word)
-            .on("click", removeWord)
-
-svg
+var bars = svg
   .selectAll("rect")
-  .data(data, d=>d.word)
+  .data(activeData, d=>d.word)
     .enter()
     .append("rect")
       .attr("x", d => xScale(d.word))
