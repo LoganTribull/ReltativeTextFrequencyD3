3bb8e27a9d62f9e6d4e5cdc1aef7e7b832d2ad4d
Logan Tribull <logantribull@Logans-MacBook-Pro.local>
2018-10-09 14:54:10 -0700

    getting words able to activate deactivate and having chart update and transition

--- app.js
+++ app.js
@@ -1,69 +1,107 @@
+// basic parameters
 const svgWidth = 500
 const svgHeight = 500
 const svgPadding = 50
+
+// basic chart with default data
 activeData= data.filter(d => d.ratio !== null)
-inactiveData = []
+activeWords = data.map(d=>d.word)
+let inactiveData = []
 
-inactivateWord= event => {
-  inactiveData.push(...activeData.filter(d=>d.word === event.word))
-  activeData = activeData.filter(d => d.word !== event.word)
-  activeWordList
-    .data(activeData, d=>d.word)
-    .exit()
-    .remove()
-    .enter()
-    .append("button")
-    .text(d=>d.word)
-    .on("click", inactivateWord)
-
-  bars
-    .data(activeData)
-    .exit()
-    .remove()
-}
-
+//svg select and style and preparing axis group tags
 var svg = d3.select("svg")
           .attr("width", svgWidth + svgPadding)
           .attr("height", svgHeight + svgPadding)
           .style("border", "1px solid blue")
 
-var activeWordList = d3.select("#active-word-list").
-selectAll("button").
-data(activeData, d=>d.word)
-.enter()
-.append("button")
-.text(d=>d.word)
-.on("click", inactivateWord)
+    svg
+          .append("g")
+          .attr("transform", "translate(0, " + (svgHeight) + ")")
+          .attr("class", "xaxis")
 
-
-var inactiveWordList = d3.select("#active-word-list").selectAll("button").data(inactiveData, d=>d.word)
-
+// x, y, scale and axis waiting on new domain
 var xScale = d3.scalePoint()
-              .domain(activeData.map(d=>d.word))
-              .range([0, svgWidth])
+               .range([0, svgWidth])
 
 var yScale = d3.scaleLinear()
-              .domain(d3.extent(activeData, d=>d.ratio))
-              .range([0, svgHeight])
+               .range([0, svgHeight])
 
 var xAxis = d3.axisBottom(xScale)
 var yAxis = d3.axisLeft(yScale)
 
-svg
-  .append("g")
-    // .attr("transform", "translate(0, " + (svgHeight) + ")")
-    .attr("transform", `translate(0, ${svgHeight})`)
-  .call(xAxis)
 
-// Filter data for null values
 
-var bars = svg
-  .selectAll("rect")
-  .data(activeData, d=>d.word)
+draw(activeData)
+fillWords(activeData, true)
+
+
+
+
+function draw(data){
+
+  //bestow domain onto x and y scale
+  xScale.domain(activeData.map(d=>d.word))
+  yScale.domain(d3.extent(activeData, d=>d.ratio))
+
+  console.log(yScale(4))
+
+  // add in the axis
+  svg.select(".xaxis").transition().duration(300)
+     .call(d3.axisBottom(xScale))
+
+  var bars = svg.selectAll("rect").data(activeData, d=> d.word)
+
+  bars.exit().remove().transition().duration(400)
+
+  var bars = svg
+    .selectAll("rect")
+    .data(activeData, d=>d.word)
+      .enter()
+      .append("rect")
+        .attr("y", d => svgHeight - yScale(d.ratio))
+        .attr("height", d => yScale(d.ratio))
+
+  bars.transition().duration(1000)
+    .attr("x", d=>xScale(d.word))
+    .attr("width", "10")
+    .attr("y", d=> svgHeight - yScale(d.ratio))
+    .attr("height", d=>yScale(d.ratio))
+}
+
+function fillWords(data, active){
+  const listId= active ? "#active-word-list":"#inactive-word-list"
+  const toggleFn = active? inactivateWord:activateWord
+  let wordList = d3.select(listId)
+    .selectAll("button")
+    .data(data, d=>d.word)
+
+  // remove unactivated words
+  wordList.exit().remove()
+
+
+  // append entered words
+  wordList
     .enter()
-    .append("rect")
-      .attr("x", d => xScale(d.word))
-      .attr("y", d => svgHeight - yScale(d.ratio))
-      .attr("width", "10")
-      .attr("height", d => yScale(d.ratio))
-      .attr("fill", "blue")
+    .append("button")
+    .text(d=>d.word)
+    .on("click", toggleFn)
+}
+
+
+function inactivateWord(event){
+  const removeWord = event.word
+  inactiveData.push(...activeData.filter(d=>d.word===removeWord))
+  activeData = activeData.filter(d => d.word !== removeWord)
+  draw(activeData)
+  fillWords(activeData, true)
+  fillWords(inactiveData, false)
+}
+
+function activateWord(event){
+  const removeWord = event.word
+  activeData.push(...inactiveData.filter(d=>d.word===removeWord))
+  inactiveData= inactiveData.filter(d => d.word !== removeWord)
+  draw(activeData)
+  fillWords(activeData, true)
+  fillWords(inactiveData, false)
+}
