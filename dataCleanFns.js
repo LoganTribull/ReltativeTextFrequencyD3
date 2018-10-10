c6566255c578700523a753133b459ae7927a7f27
Logan Tribull <logantribull@Logans-MacBook-Pro.local>
2018-10-09 19:54:07 -0700

    clean text input function which adds stop words to words to remove takes out punctuation and returns tokenized list without words to remove

--- dataCleanFns.js
+++ dataCleanFns.js
@@ -8,3 +8,10 @@ function tokenize(text, dontInclude){
   let preFilteredTokens = text.split(" ")
   return preFilteredTokens.filter(token => !dontInclude.includes(token) )
 }
+
+function cleanTextInput(text, wordsToRemove = []){
+  const stopWords = ["the", "it", "he", "she"]
+  let remove = wordsToRemove.concat(stopWords)
+  let cleanedText = removePunctuation(text)
+  return tokenize(cleanedText, remove)
+}
